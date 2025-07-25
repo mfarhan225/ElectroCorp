6e9d20020adfaf3cb2cc03bf164942c5721c17b6
MuhammadFarhan-Dev <muhammad.farhan.academic@gmail.com>
2025-07-26 03:28:56 +0700

    Menyelesaikan merge dan memperbarui route di Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,8 +7,5 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/product', 'Product::index');
-<<<<<<< HEAD
 $routes->get('/about', 'About::index');
-=======
 $routes->get('/contact', 'Contact::index');
->>>>>>> e8dcdd6dbbfe5cb181daa3878b9ec69c7777b76f
